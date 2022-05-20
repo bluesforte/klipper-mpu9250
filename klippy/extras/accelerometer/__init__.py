d30c034be43149be950af015541d2a9da887d2bc
bluesforte <harry3b9@gmail.com>
2022-05-20 13:47:20 -0700

    Update __init__.py

--- klippy/extras/accelerometer/__init__.py
+++ klippy/extras/accelerometer/__init__.py
@@ -4,10 +4,6 @@ def load_config(config):
     return accelerometer.load_config(config)
 
 def load_config_prefix(config):
-    if not config.has_section('accelerometer'):
-        raise config.error(
-            "A primary [accelerometer] section must be defined in printer.cfg "
-            "to use auxilary displays")
     name = config.get_name().split()[-1]
     if name == "accelerometer":
         raise config.error(
