ffba734ba2b5020e338f81c38aaaaca8535e8f5c
bluesforte <harry3b9@gmail.com>
2022-05-20 14:41:51 -0700

    Update motion_sensor.py

--- klippy/extras/motion_sensor/motion_sensor.py
+++ klippy/extras/motion_sensor/motion_sensor.py
@@ -2,14 +2,16 @@ import importlib
 
 sensor_chip_infos = {
         "adxl345": { "module" : ".adxl345",     "class" : "ADXL345" },
-        "mpu9250": { "module" : ".invensense",  "class" : "MPU9250" },
         "mpu6050": { "module" : ".invensense",  "class" : "MPU6050" },
+        "mpu9250": { "module" : ".invensense",  "class" : "MPU9250" },
     }
 
 class MotionSensor:
     def __init__(self, config):
         self.sensor_chip_info = config.getchoice('chip', sensor_chip_infos)
-        mod = importlib.import_module(self.sensor_chip_info["module"], package="extras.motion_sensor")
+        package_name = '.'.join(self.__module__.split('.')[:-1])
+        mod = importlib.import_module(name=self.sensor_chip_info["module"], 
+            package=package_name)
         self.sensor_class = getattr(mod, self.sensor_chip_info["class"])
         self.sensor = self.sensor_class(config)
 
