0ad3ce2e161b932d164f58c386ac8d447fc5d56e
bluesforte <harry3b9@gmail.com>
2022-05-20 13:10:41 -0700

    Update accelerometer.py

--- klippy/extras/accelerometer/accelerometer.py
+++ klippy/extras/accelerometer/accelerometer.py
@@ -1,6 +1,19 @@
+import importlib
+
+accel_chip_infos = {
+        "adxl345": { "package" : "adxl345",     "class" : "ADXL345" },
+        "mpu9250": { "package" : "invensense",  "class" : "MPU9250" },
+        "mpu6050": { "package" : "invensense",  "class" : "MPU6050" },
+    }
+
 class Accelerometer:
     def __init__(self, config):
-        pass
+        self.accel_chip_info = config.getchoice('chip', accel_chip_infos)(config)
+        mod = importlib.import_module(self.accel_chip_info["package"], ".")
+        self.accel_class = getattr(mod, self.accel_chip_info["class"])
+        self.accelerometer = self.accel_class(config)
+    
+
 
 def load_config(config):
     return Accelerometer(config)
